1ff9f41a9dd5107be0e66818d2510f335a93b737
Eoogeen <105519239+Eoogeen@users.noreply.github.com>
2023-10-20 16:32:05 +0300

    Update send.php

--- send.php
+++ send.php
@@ -13,16 +13,60 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     $url = "https://api.telegram.org/bot" . $bot_token . "/sendMessage?chat_id=" . $chat_id . "&text=" . urlencode($text);
     
-    $response = file_get_contents($url);
+    $response = file_get_contents($url); 
 
     // Возвращаем ответ клиенту (можно использовать для обработки результата)
     if ($response === false) {
-        echo json_encode(array("success" => false, "message" => "Ошибка отправки сообщения"));
+ 
     } else {
-        echo json_encode(array("success" => true, "message" => "Спасибо за Ваш отзыв!"));
+    
     }
 } else {
-    // Если запрос не является POST-запросом, вернуть ошибку
-    echo json_encode(array("success" => false, "message" => "Недопустимый метод запроса"));
+ 
 }
 ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Спасибо за отзыв</title>
+<style>
+    body {
+        margin: 0;
+        font-family: Arial, sans-serif;
+        background-color: #f4f4f4;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+
+    .thank-you {
+        background-color: #fff;
+        padding: 20px;
+        border-radius: 5px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        text-align: center;
+        animation: appear 1s ease-in-out;
+    }
+
+    @keyframes appear {
+        from {
+            opacity: 0;
+            transform: translateY(-20px);
+        }
+
+        to {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
+</style>
+</head>
+<body>
+<div class="thank-you">
+    <h1>Спасибо за отзыв!</h1>
+</div>
+</body>
+</html>
